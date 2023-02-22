3c9d4caca7eb732f20f72929e2e2e690b7721704
khayalnb_997 <xeyal.binnetov@profit.az>
2023-02-22 16:39:41 +0400

    added blog and shop page

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,27 @@ const categoryBtn = document.getElementsByClassName("wt-category")[0],
 
 categoryBtn.addEventListener("click", function () {
   categoryMenu.classList.toggle("active");
-  [...sliderBox].forEach(item=>{
-    item.classList.remove('col-sm-12');
-    item.classList.toggle('col-sm-11');
-    item.classList.toggle('ps-4');
-
-  })
+  [...sliderBox].forEach((item) => {
+    item.classList.remove("col-sm-12");
+    item.classList.toggle("col-sm-11");
+    item.classList.toggle("ps-4");
+  });
 });
 
-const heartBox=document.getElementsByClassName('heart-box');
+const heartBox = document.getElementsByClassName("heart-box");
 
-[...heartBox].forEach(item=>{
-  item.addEventListener('click',function(){
-      if(this.children[0].dataset.heart==1){
-        this.children[0].src="assets/img/redHeart.svg"
-        this.children[0].dataset.heart=2
-      }else{
-        this.children[0].src="assets/img/heart.svg"
-        this.children[0].dataset.heart=1
-      }
+[...heartBox].forEach((item) => {
+  item.addEventListener("click", function () {
+    if (this.children[0].dataset.heart == 1) {
+      this.children[0].src = "assets/img/redHeart.svg";
+      this.children[0].dataset.heart = 2;
+    } else {
+      this.children[0].src = "assets/img/heart.svg";
+      this.children[0].dataset.heart = 1;
+    }
   });
-})
+});
+
+
+
+
